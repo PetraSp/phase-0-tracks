8054bfcf52bdb966b48a66c07e6b9322345d93e1
Petra Spirkova <pspirkova@gmail.com>
2017-02-02 09:32:51 +0100

    update client information

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,11 +1,11 @@
 # - Keys should be symbols
 # - Prompt the user for client's name; age; number of children; decor theme.
 # - Convert any user input to the appropriate data type
-# - Print the hash back out to the screen when the designer has answered all of the questions. p client_information
-# - Possibility to update a key (no loop)
+# - Print the hash back out to the screen when the designer has answered all of the questions. 
+# - Ask user if they would like to update a key value pair (no loop)
+# - Process input, and update the appropriate value
 # - Print the latest version of the hash, and exit the program
 
-# Ask user for details of a given client
 client_information = {
   name: "name",
   age: "age",
@@ -30,19 +30,18 @@ puts "What's your client decor theme?"
 p client_information
 
 
-
 puts "What keys would you like to update? Please type 'none' or the key."
       input = gets.chomp 
   if input == "none"
     puts "Thanks for using the program. Bye!"
   else 
+  	 input = input.to_sym
    	puts "Please enter a new value:"
-   	  input = input.to_sym
  	  new_value = gets.chomp 
  	  if new_value == :name || :decor_theme 
  	  client_information[input] = new_value
  	  else new_value == :age || :number_of_children
  	  client_information[input] = new_value.to_i
-  end
- 	  p client_information
+  	  end
+ 	 p client_information
   end 
